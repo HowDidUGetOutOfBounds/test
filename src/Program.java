799aa0d2f9528ee5a30a492b76cbf24ac3458d92
Sergey <serg.karpik@gmail.com>
2023-04-24 19:53:02 +0300

    finished first task

---
+++ src/Program.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        /**
+         * Простая (2 балла)
+         *
+         * Поезд вышел со станции отправления в h1 часов m1 минут (например в 9:25) и
+         * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
+         * Определите время поезда в пути в минутах (в данном случае 216).
+         */
+
+        trainTask();
+
+
+    }
+
+    private static void trainTask() {
+        int h1;
+        int m1;
+        int h2;
+        int m2;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите час отправления");
+        h1 = in.nextInt();
+        System.out.println("Введите минуты отправления");
+        m1 = in.nextInt();
+        System.out.println("Введите час прибытия");
+        h2 = in.nextInt();
+        System.out.println("Введите минуты прибытия");
+        m2 = in.nextInt();
+        double result;
+        double result1;
+        result = ((h2 * 60 + m2) - (h1 * 60 + m1));
+        result1 = (((h2+24) * 60 + m2) - (h1 * 60 + m1));
+        if(h2>h1) {
+            System.out.println("Время в пути составит = " + result);
+        }
+        else{
+            System.out.println("Время в пути со сменой даты составит = " + result1);
+        }
+    }
+}
